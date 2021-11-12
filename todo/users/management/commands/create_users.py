58cbe13dc8cfcc47f09c016d7a561aaca7e80589
kroleg <kr.oleg@gmail.com>
2021-11-13 01:52:45 +0300

    Add solution for lesson 1

--- todo/users/management/commands/create_users.py
+++ todo/users/management/commands/create_users.py
@@ -1,25 +1,27 @@
-import os
-import json
 from django.core.management.base import BaseCommand
 
 from users.models import User
-
-JSON_PATH = 'users/fixtures/'
-
-
-def load_from_json(file_name):
-    with open(file_name, mode='r', encoding='utf-8') as infile:
-        return json.load(infile)
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         User.objects.all().delete()
-        User.objects.create_superuser('django2', 'django2@geekshop.local', 'GeekBrains122@')
-        my_users = load_from_json(JSON_PATH + 'users.json')
-        
-        for u in my_users:
-            user_fields = u.get('fields')
-            user_fields['id'] = u.get('pk')
-            new_user = User(**user_fields)
-            new_user.save()
+
+        User.objects.create_superuser(username='ok',
+                                      first_name='ok',
+                                      last_name='super_ok',
+                                      email='django2@geekshop.local',
+                                      password='1')
+
+        User.objects.create_user(username='testUser',
+                                 first_name='testUser',
+                                 last_name='testUserOff',
+                                 email='testUser@geekshop.local',
+                                 password='GeekBrains122@')
+
+        User.objects.create_user(username='testUser2',
+                                 first_name='testUser',
+                                 last_name='testUserTwo',
+                                 email='testoff@geekshop.local',
+                                 password='GeekBrains122@')
+
