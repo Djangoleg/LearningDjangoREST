b00aab9314904f3f05a934514e007cdb2f00063e
kroleg <kr.oleg@gmail.com>
2021-11-20 01:15:07 +0300

    Added app user

---
+++ todo/frontend/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react'
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            'users': []
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                Main App
+            </div>
+        )
+    }
+}
+
+export default App;
