b9be784af846149a1d94c9f0f9c8ccecca561052
kroleg <kr.oleg@gmail.com>
2021-11-20 12:32:37 +0300

    Added header, footer and style for them

--- todo/frontend/src/App.js
+++ todo/frontend/src/App.js
@@ -14,23 +14,43 @@ class App extends React.Component {
 
     componentDidMount() {
         axios.get('http://127.0.0.1:8000/api/users/')
-       .then(response => {
-           const users = response.data
-               this.setState(
-               {
-                   'users': users
-               }
-           )
-       }).catch(error => console.log(error))
-   }
+            .then(response => {
+                const users = response.data
+                this.setState(
+                    {
+                        'users': users
+                    }
+                )
+            }).catch(error => console.log(error))
+    }
 
-   render () {
-       return (
-           <div>
-               <UserList users={this.state.users} />
-           </div>
-       )
-   }
+    render() {
+        return (
+            <div className="wrapper">
+
+                <div className="header">
+                    <a className="active" href="index.html">Home</a>
+                    <a href="#news">News</a>
+                    <a href="#contact">Contact</a>
+                    <a href="#about">About</a>
+                </div>
+
+                <div className="content">
+                    <div className="contentDiscription">
+                        <b>Пользователи</b>
+                    </div>
+                    <UserList users={this.state.users}/>
+                </div>
+
+                <div className="footer">
+                    <div className="footerText">
+                        ©{(new Date().getFullYear())} by OK
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
 
 }
 
