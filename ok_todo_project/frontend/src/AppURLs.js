6fe40a0e49c5f1769adba73729954c39faeec943
kroleg <kr.oleg@gmail.com>
2021-12-26 14:22:51 +0300

    Front-end reconfiguration to localhost

--- ok_todo_project/frontend/src/AppURLs.js
+++ ok_todo_project/frontend/src/AppURLs.js
@@ -1,14 +1,14 @@
-// const app_urls = {
-//         'api_token_auth': 'http://127.0.0.1:8000/api-token-auth/',
-//         'users': 'http://127.0.0.1:8000/api/users/',
-//         'projects': 'http://127.0.0.1:8000/api/projects/',
-//         'todo': 'http://127.0.0.1:8000/api/todo/',
-//     };
 const app_urls = {
-        'api_token_auth': 'http://10.3.3.25:8080/api-token-auth/',
-        'users': 'http://10.3.3.25:8080/api/users/',
-        'projects': 'http://10.3.3.25:8080/api/projects/',
-        'todo': 'http://10.3.3.25:8080/api/todo/',
+        'api_token_auth': 'http://127.0.0.1:8000/api-token-auth/',
+        'users': 'http://127.0.0.1:8000/api/users/',
+        'projects': 'http://127.0.0.1:8000/api/projects/',
+        'todo': 'http://127.0.0.1:8000/api/todo/',
     };
+// const app_urls = {
+//         'api_token_auth': 'http://10.3.3.25:8080/api-token-auth/',
+//         'users': 'http://10.3.3.25:8080/api/users/',
+//         'projects': 'http://10.3.3.25:8080/api/projects/',
+//         'todo': 'http://10.3.3.25:8080/api/todo/',
+//     };
 
 export default app_urls
